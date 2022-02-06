4981507432b737f86d537931cb91edd857801cd1
ben-m-j-taylor <11777727+ben-m-j-taylor@users.noreply.github.com>
2022-02-06 01:19:54 +0000

    Adding input dialog to get the name to use in the generator

--- src/generator/generator.ts
+++ src/generator/generator.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 
 import { getConfig, validateConfig } from './config';
 
-const generate = (location: string, newDirectory: boolean) => {
+const generate = async (location: string, newDirectory: boolean) => {
   const config = getConfig();
 
   let { valid, validationErrors } = validateConfig(config);
@@ -16,6 +16,15 @@ const generate = (location: string, newDirectory: boolean) => {
     vscode.window.showInformationMessage('vscode-file-gen: Generating files in directory!');
   } else {
     vscode.window.showInformationMessage('vscode-file-gen: Generating new directory with files!');
+  }
+
+  const specifiedName = await vscode.window.showInputBox({
+    title: `Enter the name you would like to use in ${config.directoryNameCasing}`,
+    placeHolder: config.directoryNameCasing || ''
+  });
+
+  if (!specifiedName) {
+    return;
   }
 };
 
